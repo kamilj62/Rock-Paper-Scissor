a1dd83e525570de8831c7deed230c5135ce54ceb
Joseph Kamil <kamilj@umich.edu>
2024-01-08 22:37:38 -0800

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -8,13 +8,21 @@ var options = ['R', 'P', 'S'];
 
 var playGame = function () {
 	// TODO: Use the prompt() method to ask the user for their choice between 'R', 'P', and 'S' and store the response in a variable named 'userChoice'
+	var userChoice = prompt("choose between 'R', 'P', and 'S'");
 
 	// TODO: Randomly select an element from the array 'options' and store it in a variable named 'computerChoice' - Google for example: "How can I select a random element from an array in JavaScript?"
+	var computerChoice = options[Math.floor(Math.random() * options.length)];
 
 	// TODO: Use the alert() method to display a message to let the user know what the computer has chosen
-
+	alert(computerChoice);
 	// TODO: Write a conditional statement to determine the outcome:
+	if (userChoice === computerChoice) {
+		alert("tie");
+		tie++;
+	}
+
 	/*
+	
   - if both userChoice and computerChoice are the same, increment the variable 'ties' by 1 and use the alert() method to let the user know it's a tie
   - else if the user has won, increment the variable 'wins' by 1 and use the alert() method to let the user know
   - else the user lost: increment the variable 'losses' by 1 and use the alert() method to let the user know
